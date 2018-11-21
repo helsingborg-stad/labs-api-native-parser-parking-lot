41efcb4ffa88f68d7c3f7b041405717646c57fa6
magnus weidmar <magnus.weidmar@sogeti.se>
2018-11-21 18:27:17 +0100

    Parking API call

---
+++ src/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const SwaggerExpress = require('swagger-express-mw');
+const app = require('express')();
+const axios = require('axios');
+const swaggerUi = require('swagger-ui-express');
+module.exports = app; // for testing
+
+const config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  // install middleware
+  swaggerExpress.register(app);
+
+  const port = process.env.PORT || 3000;
+  app.listen(port);
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerExpress.runner.swagger));
+});
---
+++ src/services/parkingLotService.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const { stringify } = require('flatted');
+
+export const fetchParkigsInDistanceRadiusFromPosition = (lat, lon, dist) => {
+    return new Promise(function(resolve, reject) {
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Accept': 'application/json'
+            }
+        };
+
+        const baseUrl = 'https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=parkering_new&facet=plats&facet=status&geofilter.distance=';
+        axios.get(`${baseUrl}${lat}%2C+${lon}%2C+${dist}`, config)
+            .then(result => {
+                resolve(result.data.records);
+            })
+            .catch(error => {
+                reject(error);
+            })
+    })
+};
