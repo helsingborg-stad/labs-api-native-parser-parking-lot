327533c5d54e7cd85b94a3ed8132562ca0e46a2d
Magnus-Weidmar <33689928+Magnus-Weidmar@users.noreply.github.com>
2018-12-13 15:09:48 +0100

    Sprint 0 (#8)
    * iisnode config
    
    * trigger test
    
    * trigger test
    
    * trigger test 2
    
    * removed strict
    
    * Added config to build
    
    * add file
    
    * Added swagger dist files to root
    
    * Added swagger dist files to root
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * static root to app.js
    
    * cleaned up from iisnode wrapping

--- src/app.js
+++ src/app.js
@@ -2,22 +2,21 @@
 
 const SwaggerExpress = require('swagger-express-mw');
 const app = require('express')();
-const axios = require('axios');
 const swaggerUi = require('swagger-ui-express');
 module.exports = app; // for testing
 
 const config = {
-  appRoot: __dirname // required config
+    appRoot: __dirname // required config
 };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+    if (err) { throw err; }
 
-  // install middleware
-  swaggerExpress.register(app);
+    // install middleware
+    swaggerExpress.register(app);
 
-  const port = process.env.PORT || 3000;
-  app.listen(port);
+    const port = process.env.PORT || 3000;
+    app.listen(port);
 
-  app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerExpress.runner.swagger));
+    app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerExpress.runner.swagger));
 });
